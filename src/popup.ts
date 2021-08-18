916c641e85f480f7a1dee705bfac0fc5b2c3d78d
Matthbo <matthijs.booman@hotmail.com>
2021-08-18 02:21:53 +0200

    added typescript support, basic html/css/ts files

---
+++ src/popup.ts
@@ -0,0 +1,7 @@
+const btn = document.getElementById("action");
+
+btn?.addEventListener("click", () => {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+});
+
