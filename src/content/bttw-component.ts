7308b3dc4a6d1fbfc25b7ac866bc9ff012b78447
Matthbo <matthijs.booman@hotmail.com>
2021-08-27 02:35:55 +0200

    Proper working toggle button & modal web component

--- src/content/bttw-component.ts
+++ src/content/bttw-component.ts
@@ -1,14 +1,17 @@
-import { css, html, LitElement } from "lit";
-import { customElement } from "lit/decorators.js";
+import { css, html, LitElement, PropertyValues } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { log } from "./utils";
 
 @customElement("bttw-component")
 export class BTTWComponent extends LitElement {
+    @property({ type: Boolean/* , attribute: "shown"  */})
+    shown: boolean = false;
 
     static styles = css`
         div.wrapper {
-            position: absolute;
-            width: 500px;
-            height: 500px;
+            position: fixed;
+            width: calc(100vw / 4 * 3);
+            height: calc(100vh / 4 * 3);
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
@@ -18,12 +21,16 @@ export class BTTWComponent extends LitElement {
         }
     `;
 
+    update(ChangedProperties: PropertyValues){
+        super.update(ChangedProperties);
+        log(ChangedProperties);
+    }
+
     render(){
-        return html`
+        return this.shown ? html`
             <div class="wrapper">
                 <p>Test component</p>
             </div>
-        `;
+        ` : null;
     }
-
 }
--- src/content/main.ts
+++ src/content/main.ts
@@ -1,5 +1,5 @@
 import browser from "webextension-polyfill";
-import { log } from "./test";
+import { log } from "./utils";
 import "./bttw-component";
 
 class BetterTwitter {
@@ -29,24 +29,38 @@ class BetterTwitter {
     }
 
     private initialise() {
-        const elems = document.getElementsByClassName("css-1dbjc4n r-1awozwy r-18u37iz r-1iud8zs r-1777fci r-1jgb5lz r-sb58tz r-1j3t67a r-13qz1uu");
-        const menubar = Array.from(elems as HTMLCollectionOf<HTMLElement>).find(elem => elem.innerText == "Home");
+        // TODO create a wait for element util function so that it makes sure the menubar exists before trying to add icon
+
+        const rootDiv = document.querySelector<HTMLElement>("#react-root");
+        const bttwConponentInject = document.createElement("bttw-component");
+        rootDiv?.appendChild(bttwConponentInject);
 
         const menuIconWrapper = document.createElement("div");
         menuIconWrapper.className = "css-1dbjc4n r-obd0qt r-1pz39u2 r-1777fci r-1vsu8ta r-2j7rtt";
         menuIconWrapper.innerHTML = `<p>🎨</p>`;
         menuIconWrapper.id = "toggleBTTW";
         menuIconWrapper.onclick = function() {
-            log("clicked!!!!");
-            const rootDiv = document.querySelector("#react-root");
-            const bttwConponentInject = document.createElement("bttw-component");
-            rootDiv?.appendChild(bttwConponentInject);
+            /* appBasis used because everything doesn't shift from here */
+            const appBasis = document.querySelector<HTMLElement>(".css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010");
+            const isShown = document.querySelector("bttw-component")?.getAttribute("shown") != null;
+
+            if (isShown){
+                document.querySelector("bttw-component")?.removeAttribute("shown");
+                const scrollY = appBasis!.style.top;
+                appBasis!.style.position = '';
+                appBasis!.style.top = '';
+                window.scrollTo(0, parseInt(scrollY || '0') * -1);
+            } else {
+                appBasis!.style.top = `-${window.scrollY}px`;
+                appBasis!.style.position = 'fixed';
+                document.querySelector("bttw-component")?.setAttribute("shown", "");
+            }
         }
-
-        log(menuIconWrapper);
-        (menubar?.lastChild as HTMLElement)?.classList.remove("r-2j7rtt");
-
+        
+        const elems = document.getElementsByClassName("css-1dbjc4n r-1awozwy r-18u37iz r-1iud8zs r-1777fci r-1jgb5lz r-sb58tz r-1j3t67a r-13qz1uu");
+        const menubar = Array.from(elems as HTMLCollectionOf<HTMLElement>).find(elem => elem.innerText == "Home");
         menubar?.insertBefore(menuIconWrapper, menubar.lastChild);
+        (menubar?.lastChild as HTMLElement | undefined)?.classList.remove("r-2j7rtt");
     }
 }
 
